22c5d702b8ebca33b9ef466c5225bd2a711727c5
박종윤 <55042337+JJongmen@users.noreply.github.com>
2023-09-06 00:06:51 +0900

    Chap02 | TDD 시작 (#2)
    * 1. move StringTest
    
    * 2. create Calculator.plus
    
    * 3. 모든 규칙을 충족하는 경우 테스트 통과
    
    * 4. 길이만 8글자 미만이고 나머지 조건은 충족하는 경우
    
    * 5. 숫자를 포함하지 않고 나머지 조건은 충족하는 경우
    
    * 6. 테스트 코드 리팩터링
    
    * 7. 값이 없는 경우
    
    * 8. 대문자를 포함하지 않고 나머지 조건을 충족하는 경우
    
    * 9. 길이가 8글자 이상인 조건만 충족하는 경우
    
    * 10. 숫자 포함 조건만 충족하는 경우
    
    * 11. 대문자 포함 조건만 충족하는 경우
    
    * 12. meter() 메서드 리팩터링
    
    * 13. 아무 조건도 충족하지 않은 경우
    
    * 14. 코드 정리: 코드 가독성 개선
    
    * 15. 테스트에서 메인으로 코드 이동

---
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -0,0 +1,37 @@
+package chap02;
+
+public class PasswordStrengthMeter {
+    public PasswordStrength meter(final String s) {
+        if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
+        int metCounts = getMetCriteriaCounts(s);
+        if (metCounts <= 1) return PasswordStrength.WEAK;
+        if (metCounts == 2) return PasswordStrength.NORMAL;
+        return PasswordStrength.STRONG;
+    }
+
+    private int getMetCriteriaCounts(final String s) {
+        int metCounts = 0;
+        if (s.length() >= 8) metCounts++;
+        if (meetsContainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUppercaseCriteria(s)) metCounts++;
+        return metCounts;
+    }
+
+    private boolean meetsContainingUppercaseCriteria(final String s) {
+        for (final char ch : s.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean meetsContainingNumberCriteria(final String s) {
+        for (final char ch : s.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/test/java/chap01/StringTest.java
+++ src/test/java/chap01/StringTest.java
@@ -1,3 +1,5 @@
+package chap01;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
