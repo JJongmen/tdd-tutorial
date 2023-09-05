a93987c4f9dc1b941d4209fab07466c542c83e2b
JJongmen <pjy90123@gmail.com>
2023-09-05 23:55:17 +0900

    9. 길이가 8글자 이상인 조건만 충족하는 경우

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -5,14 +5,20 @@ public class PasswordStrengthMeter {
         if (s == null || s.isEmpty()) {
             return PasswordStrength.INVALID;
         }
-        if (s.length() < 8) {
-            return PasswordStrength.NORMAL;
+        final boolean lengthEnough = s.length() >= 8;
+        final boolean containsNum = meetsContainingNumberCriteria(s);
+        final boolean containsUpp = meetsContainingUppercaseCriteria(s);
+
+        if (lengthEnough && !containsNum && !containsUpp) {
+            return PasswordStrength.WEAK;
         }
-        boolean containsNum = meetsContainingNumberCriteria(s);
+
         if (!containsNum) {
             return PasswordStrength.NORMAL;
         }
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        if (!lengthEnough) {
+            return PasswordStrength.NORMAL;
+        }
         if (!containsUpp) {
             return PasswordStrength.NORMAL;
         }
