cf29156b3e98f1da35dbb356a5c3eb6495c11f6c
JJongmen <pjy90123@gmail.com>
2023-09-06 00:03:22 +0900

    14. 코드 정리: 코드 가독성 개선

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -2,30 +2,22 @@ package chap02;
 
 public class PasswordStrengthMeter {
     public PasswordStrength meter(final String s) {
-        if (s == null || s.isEmpty()) {
-            return PasswordStrength.INVALID;
-        }
-        int metCounts = 0;
-        if (s.length() >= 8) {
-            metCounts++;
-        }
-        if (meetsContainingNumberCriteria(s)) {
-            metCounts++;
-        }
-        if (meetsContainingUppercaseCriteria(s)) {
-            metCounts++;
-        }
-
-        if (metCounts <= 1) {
-            return PasswordStrength.WEAK;
-        }
-        if (metCounts == 2) {
-            return PasswordStrength.NORMAL;
-        }
+        if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
+        int metCounts = getMetCriteriaCounts(s);
+        if (metCounts <= 1) return PasswordStrength.WEAK;
+        if (metCounts == 2) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
     }
 
-    private static boolean meetsContainingUppercaseCriteria(final String s) {
+    private int getMetCriteriaCounts(final String s) {
+        int metCounts = 0;
+        if (s.length() >= 8) metCounts++;
+        if (meetsContainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUppercaseCriteria(s)) metCounts++;
+        return metCounts;
+    }
+
+    private boolean meetsContainingUppercaseCriteria(final String s) {
         for (final char ch : s.toCharArray()) {
             if (Character.isUpperCase(ch)) {
                 return true;
@@ -34,7 +26,7 @@ public class PasswordStrengthMeter {
         return false;
     }
 
-    private static boolean meetsContainingNumberCriteria(final String s) {
+    private boolean meetsContainingNumberCriteria(final String s) {
         for (final char ch : s.toCharArray()) {
             if (ch >= '0' && ch <= '9') {
                 return true;
