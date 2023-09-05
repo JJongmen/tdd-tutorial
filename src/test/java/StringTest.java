689eeb3e5bd895823d2aeb661a42b4a13ac7f5c2
박종윤 <55042337+JJongmen@users.noreply.github.com>
2023-09-05 22:48:40 +0900

    Chap01 TDD 개발 준비 #1

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    void substring() {
+        String str = "abcde";
+        assertEquals("cd", str.substring(2, 4));
+    }
+}
