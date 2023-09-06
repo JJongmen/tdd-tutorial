071746dcce2f4c3d0b0986b6138ff440b6ebb56b
JJongmen <pjy90123@gmail.com>
2023-09-06 20:18:33 +0900

    7. 첫 납부일과 만료일 일자가 다를 때 2만원 이상 납부 사례 추가

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -1,19 +1,36 @@
 package chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(final PayData payData) {
         final int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            final LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if (payData.getFirstBillingDate().getDayOfMonth() !=
-                    candidateExp.getDayOfMonth()) {
-                return candidateExp.withDayOfMonth(
-                        payData.getFirstBillingDate().getDayOfMonth()
-                );
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
         }
         return payData.getBillingDate().plusMonths(addedMonths);
+    }
+
+    private LocalDate expiryDateUsingFirstBillingDate(final PayData payData, final int addedMonths) {
+        final LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+        if (isSameDayOfMonth(payData.getFirstBillingDate(), candidateExp)) {
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if (dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
+    }
+
+    private int lastDayOfMonth(final LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
+    }
+
+    private boolean isSameDayOfMonth(final LocalDate date1, final LocalDate date2) {
+        return date1.getDayOfMonth() != date2.getDayOfMonth();
     }
 }
