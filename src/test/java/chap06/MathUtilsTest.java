e271edeffd3d786ed1049def4b7c4164b0c1127d
JJongmen <pjy90123@gmail.com>
2023-09-07 13:58:07 +0900

    1. 명시적으로 파일이 없는 상황 만들기

---
+++ src/test/java/chap06/MathUtilsTest.java
@@ -0,0 +1,29 @@
+package chap06;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+    @Test
+    void noDataFile_Then_Exception() {
+        givenNoFile("badpath.txt");
+
+        File dataFile = new File("badpath.txt");
+        assertThrows(IllegalArgumentException.class,
+                () -> MathUtils.sum(dataFile));
+    }
+
+    private void givenNoFile(final String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            boolean deleted = file.delete();
+            if (!deleted) {
+                throw new RuntimeException("fail givenNoFile: " + path);
+            }
+        }
+    }
+}
