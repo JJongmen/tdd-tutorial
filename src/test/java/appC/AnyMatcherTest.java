c9918615badc05003c14b870d0684201e41afa01
JJongmen <pjy90123@gmail.com>
2023-09-07 20:14:46 +0900

    6. 임의 값 일치와 정확한 값 일치가 필요한 경우 eq() 메서드를 사용

--- src/test/java/appC/AnyMatcherTest.java
+++ src/test/java/appC/AnyMatcherTest.java
@@ -2,8 +2,10 @@ package appC;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -18,5 +20,15 @@ public class AnyMatcherTest {
 
         final String num2 = genMock.generate(GameLevel.NORMAL);
         assertEquals("456", num2);
+    }
+
+    @Test
+    void mixAnyAndEq() {
+        List<String> mockList = mock(List.class);
+
+        given(mockList.set(anyInt(), eq("123"))).willReturn("456");
+
+        final String old = mockList.set(5, "123");
+        assertEquals("456", old);
     }
 }
