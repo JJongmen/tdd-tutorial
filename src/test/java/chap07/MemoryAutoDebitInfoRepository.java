1d9d626ae14647c3b449e21de0b75fc000ae9a86
JJongmen <pjy90123@gmail.com>
2023-09-07 15:24:10 +0900

    5. 대역을 사용하여 DB 없이 테스트

---
+++ src/test/java/chap07/MemoryAutoDebitInfoRepository.java
@@ -0,0 +1,19 @@
+package chap07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryAutoDebitInfoRepository implements AutoDebitInfoRepository {
+
+    private Map<String, AutoDebitInfo> infos = new HashMap<>();
+
+    @Override
+    public void save(final AutoDebitInfo info) {
+        infos.put(info.getUserId(), info);
+    }
+
+    @Override
+    public AutoDebitInfo findOne(final String userId) {
+        return infos.get(userId);
+    }
+}
