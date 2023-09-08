a135d1ffef4737047d9b35328e01fd18a7f44ac3
JJongmen <pjy90123@gmail.com>
2023-09-08 16:31:25 +0900

    2. 정적 메서드를 사용하는 경우 테스트가 어렵다

---
+++ src/test/java/chap08/auth/AuthUtil.java
@@ -0,0 +1,11 @@
+package chap08.auth;
+
+public class AuthUtil {
+    public static boolean authorize(final String authKey) {
+        return false;
+    }
+
+    public static int authenticate(final String id, final String pw) {
+        return 0;
+    }
+}
